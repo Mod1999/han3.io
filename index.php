facbb06136d60a9ed96d1f4ed6f05de3c807093e
HAN <lolo-amx2@mail.ru>
2016-08-31 07:28:32 +0300

    222

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+/*==========================*\
+# Clone Coded by AtakanCan   #
+\*==========================*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!--
+        Youtube Converter - Youtube Downloader
+        Copyright 2014 AtakanCan - All rights reserve
+        You are NOT allowed to remove or change
+        the position of this copyright.
+    -->
+
+    <meta charset="windows-1251">
+    <meta name="generator" content="AtakanCan - Youtube Converter" />
+    <title>Скачать видео с YOUTUBE.COM</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/pages/signin.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+
+
+<div class="account-container register">
+    <div class="content clearfix">
+        <form action="get_file.php" method="post">
+            <h1 style="text-align: center">Скачать видео с YOUTUBE.COM</h1>
+            
+
+            <div class="login-fields">
+                <p><strong>Вставьте код видео после ?v=</strong><br/>
+                  <strong>Например:</strong> http://www.youtube.com/watch?v=<strong>ASO_zypdnsQ</strong>
+                </p>
+
+                <div class="field">
+                <label for="videoid">First Name:</label>
+                    <input required type="text" id="videoid" name="videoid" value="" placeholder="Код видео"/>
+                </div>
+            </div>
+<center>
+            <div class="login-actions">
+                <button class="button btn btn-primary btn-large" style="text-align: center">Скачать</button>
+            </div>
+            <p>
+
+
+</center></p>
+        </form>
+    </div>
+</div><br><br><center>
+
+</center>
+
+
+
+<script src="js/jquery-1.7.2.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      <div style="display: none;"><a href="http://mixlip.ru/">MixliP-Все для вебмастера! У нас вы найдете скрипты uCoz , сайта ,Dle ,шаблоны,букса,скрипты хайпов,казино и многое другое</a></div>
+
+</html>
